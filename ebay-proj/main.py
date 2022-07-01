cb93b7dc6a96c81f15f15bdb1fbfdd0a75387cbd
Albert Terc <albytterc@gmail.com>
2022-07-01 18:52:16 +0000

    added outline

--- ebay-proj/main.py
+++ ebay-proj/main.py
@@ -1,1 +1,15 @@
-print("hello world")
+import requests
+import json
+from bs4 import BeautifulSoup
+
+
+# setup beautifulsoup with headers
+# get webpage with get request
+# pass webpage response to beautiful soup
+# setup authentication with the ebay api (oauth2.0)
+# get access tokens, application and user
+# Ask user for URL 
+# extract data from url with get request
+# prepare data for post request 
+# send post request to ebay with listing data
+# save details into sql database
