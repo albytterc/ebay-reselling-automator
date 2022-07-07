b2eb15a54c8f51457115cb7529b8f7f99e56ae88
Emmanuel Kebede <emmanuelemrias@gmail.com>
2022-07-07 15:08:04 -0400

    created a disctionary from the item details

--- scrapper.py
+++ scrapper.py
@@ -10,53 +10,49 @@ URL = "https://www.amazon.com/Moto-Power-Unlocked-Smartphone-T-Mobile/dp/B08L5MN
 
 HEADERS = ({'User-Agent':
            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
-                           'Accept-Language': 'en-US, en;q=0.5'})
- 
+            'Accept-Language': 'en-US, en;q=0.5'})
+
 # Send requests
 webpage = requests.get(URL, headers=HEADERS)
 soup = BeautifulSoup(webpage.content, "html.parser")
 
 if webpage.ok:
-
-
-
     # Extract item name
     def get_name():
-        title = soup.find("span",attrs={"id":'productTitle'})
+        title = soup.find("span", attrs={"id": 'productTitle'})
         title_value = title.string
         title_string = title_value.strip().replace(',', '')
         return(title_string)
 
+    # Extract price
 
-    # # Extract price
     def get_price():
         price = soup.find("span", attrs={"class": 'a-offscreen'})
         price_value = price.string
         return(price_value)
 
+    # Extract item description
 
-    # # Extract item description
-    # # TODO: clean up text s
     def get_description():
-        desc = soup.find_all('ul', {"class": "a-unordered-list a-vertical a-spacing-mini"})
+        desc = soup.find_all(
+            'ul', {"class": "a-unordered-list a-vertical a-spacing-mini"})
         describtion = []
         for i in desc:
             if i.find_all("span", {"class": "a-list-item"}):
                 for j in i:
                     describtion.append(j.string)
-        
+
         for i in describtion:
             if i == '' or i == '\n' or i == ' ':
                 describtion.remove(i)
-        
+
         # Get rid of None and irrelevant details
         describtion = [i for i in describtion if i]
         describtion.pop(0)
 
         return describtion
 
-
-    # # Extract pictures
+    # Extract pictures
     def get_pictures():
         pic = soup.findAll("span", {"class": "a-button-text"})
         pics = []
@@ -66,10 +62,39 @@ if webpage.ok:
         pics.pop()
         return pics
 
- 
+    # Extract Details
+
+    def get_details():
+        details = {}
+
+        # Get Key
+        key_unfiltered = soup.findAll(
+            "span", {"class": "a-size-base a-text-bold"})
+        key = []
+        for i in key_unfiltered:
+            key.append(i.string)
+
+        key = [i for i in key if i]
+        key.pop(0)
+
+        # Get Value
+        val_unfiltered = soup.findAll("td", {"class": "a-span9"})
+        val = []
+        for i in val_unfiltered:
+            if i.find("span", {"class": "a-size-base"}):
+                for j in i:
+                    val.append(str(j.string))
+
+        for i in val:
+            if i == ' ' or len(i) < 2:
+                val.remove(i)
+
+        val = list(filter(str.strip, val))
+
+        # create a dictionary with the key and val
+        for i in range(len(key)):
+            details[key[i]] = val[i]
+
+        return details
 else:
     print("Error with URL")
-
-print(get_pictures())
-
-# print(desc)
